f100218a419d905b083bb2d8eb6e8472b74a16e8
Aaron Bieber <aaron@bolddaemon.com>
2019-07-26 21:44:43 -0600

    initial bits for dotBeatBar \o/

--- dotBeatBar_old/AppDelegate.swift
+++ dotBeatBar_old/AppDelegate.swift
@@ -7,20 +7,31 @@
 //
 
 import Cocoa
+import Beat
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    var timer = Timer()
+    let si = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+    @objc func updateStatBar(_ sender: Any?) {
+        if let button = si.button {
+            button.title = "@\(Beat(Date().timeIntervalSince1970).text())"
+        }
+    }
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        updateStatBar(self)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updateStatBar), userInfo: nil, repeats: true)
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action:
+            #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        si.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        timer.invalidate()
     }
-
-
 }
-
