82a237bfcf527fb27962d5cdd5780ad844236337
Aaron Bieber <aaron@bolddaemon.com>
2021-01-01 15:56:01 -0700

    Rename to dotBeatBar (for most stuff).
    - Remove ssh-agent identities when locking.
    - Disable app sandbox (this lets us remove the agent).

--- dotBeatBar_old/AppDelegate.swift
+++ dotBeatBar_old/AppDelegate.swift
@@ -12,13 +12,17 @@ import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let dnc = DistributedNotificationCenter.default()
     var timer = Timer()
+    
     let si = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+    
     @objc func updateStatBar(_ sender: Any?) {
         if let button = si.button {
             button.title = "@\(Beat().text())"
         }
     }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         updateStatBar(self)
         timer = Timer.scheduledTimer(timeInterval: 1,
@@ -28,14 +32,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                      repeats: true)
 
         let menu = NSMenu()
+
         menu.addItem(NSMenuItem(title: "Quit", action:
             #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
         si.menu = menu
+        
+        _ = dnc.addObserver(forName: .init("com.apple.screenIsLocked"),
+                                       object: nil, queue: .main) { _ in
+            NSLog("Screen Locked. Removing ssh-agent keys.")
+            let task = Process()
+            let pipe = Pipe()
+            
+            task.executableURL = URL(fileURLWithPath: "/usr/bin/ssh-add")
+            task.arguments = ["-D"]
+            task.standardOutput = pipe
+            task.launch()
+            
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+
+            NSLog(output! as String)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
         timer.invalidate()
     }
 }
